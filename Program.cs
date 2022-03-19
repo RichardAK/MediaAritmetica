fd2c9013aeff570de479c4be701cc6e70b484cc7
Kauã <kau.lira@yahoo.com>
2022-03-18 22:38:26 -0300

    tarefa feita

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double n1, n2, n3, media ;
+
+Console.WriteLine("Média Aritmética de 3 números\n");
+
+Console.Write("digite o primeiro numero:");
+n1 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("digite o segundo numero:");
+n2 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("digite o terceiro numero:");
+n3 = Convert.ToDouble(Console.ReadLine()!);
+
+media = (n1+n2+n3)/3;
+
+Console.WriteLine($"Média:{media:N1}");
